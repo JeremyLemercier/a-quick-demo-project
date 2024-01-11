8b1a6dcc854307e2df54ec3217633b3805a3fb52
JeremyLemercier <jeremy.lemercier.pro@pm.me>
2024-01-11 15:15:48 -0300

    Update ci workflow for tests

---
+++ client.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+const URI= MONGO_LOCAL_URI='mongodb://127.0.0.1:27017/test-app'
+
+async function main() {
+    await connect();
+    await sleep();
+    await disconnect();
+}
+
+main();
+
+function connect() {
+    return mongoose.connect(URI, {
+        serverSelectionTimeoutMS: 5000
+      }).then(() => {
+        console.log("Database connected.");
+    }).catch(err => console.log(err.reason));
+}
+
+function disconnect() {
+    return mongoose.disconnect().then(() => {
+        console.log("Database disconnected.");
+    });
+}
+
+
+function sleep() {
+    return new Promise(resolve => {
+        for (let i = 5; i >= 1; i--) {
+            setTimeout(() => {
+                console.log('alarm in :' + i);
+                if (i === 1) {
+                    console.log('Wake up!');
+                    resolve();
+                }
+            }, (5 - i) * 1000);
+        }
+    });
+}
