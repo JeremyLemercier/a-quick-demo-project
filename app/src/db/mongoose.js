07d15e6057015852b5b6d1cb4663a3620e55b1be
Jeremy Lemercier <132686267+JeremyLemercier@users.noreply.github.com>
2024-01-11 18:41:36 -0300

    Update mongoose.js

--- app/src/db/mongoose.js
+++ app/src/db/mongoose.js
@@ -1,25 +1,22 @@
 const mongoose = require('mongoose');
-const env = process.env.ENV; 
 let MONGO_URI = '';
-
-console.log('[LOG00 - mongoose.js - ] current ENV from CI workflow: ' + env);
-
 console.log('[LOG01 - mongoose.js - ] current MONGO_LOCAL_URI : ' + process.env.MONGO_LOCAL_URI);
 console.log('[LOG02 - mongoose.js - ] current MONGO_HOST : ' + process.env.MONGO_HOST);
 console.log('[LOG03 - mongoose.js - ] current MONGO_PORT : ' + process.env.MONGO_PORT);
 
 // Connection to MongoDB without Automated Tests
 async () => {
-    MONGO_URI = setEnvConfig();
+    MONGO_URI = setEnvConfig(process.env.ENV);
     env !== 'ci' ? await connect(MONGO_URI) : null;
+    console.log('[LOG00 - mongoose.js - ] current ENV from CI workflow: ' + env);
     console.log('[LOG01 - mongoose.js] current URI : ' + MONGO_URI);
 }
 
 
 function setEnvConfig(env) {
-    console.log('[LOG01 - mongoose.js - setEnvConfig function] current MONGO_LOCAL_URI : ' + process.env.MONGO_LOCAL_URI);
-    console.log('[LOG02 - mongoose.js - setEnvConfig function] current MONGO_HOST : ' + process.env.MONGO_HOST);
-    console.log('[LOG03 - mongoose.js - setEnvConfig function] current MONGO_PORT : ' + process.env.MONGO_PORT);
+    // console.log('[LOG01 - mongoose.js - setEnvConfig function] current MONGO_LOCAL_URI : ' + process.env.MONGO_LOCAL_URI);
+    // console.log('[LOG02 - mongoose.js - setEnvConfig function] current MONGO_HOST : ' + process.env.MONGO_HOST);
+    // console.log('[LOG03 - mongoose.js - setEnvConfig function] current MONGO_PORT : ' + process.env.MONGO_PORT);
     if(env !== 'ci') {
         uri = process.env.MONGO_LOCAL_URI;
     } else if (env == 'ci') {
